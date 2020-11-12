185f809d7215a6f0a87fffbace7802472895456d
bonustrack <fabien@bonustrack.co>
2020-11-12 23:45:31 +0700

    Add Pinata pinning

--- server/utils.ts
+++ server/utils.ts
@@ -1,22 +1,42 @@
 import fetch from 'node-fetch';
+import pinataSDK from '@pinata/sdk';
 import fleek from '@fleekhq/fleek-storage-js';
 import { jsonToGraphQLQuery } from "json-to-graphql-query";
 
-const config: any = {
-  apiKey: process.env.FLEEK_API_KEY,
-  apiSecret: process.env.FLEEK_API_SECRET,
-  bucket: 'balancer-team-bucket'
-};
-
 export const ipfsNode = process.env.IPFS_NODE || 'cloudflare-ipfs.com';
 
-export async function ipfsPin(key: string, body) {
-  let ipfsHash: string;
+export async function ipfsPinFleek(key, body) {
+  const config: any = {
+    apiKey: process.env.FLEEK_API_KEY,
+    apiSecret: process.env.FLEEK_API_SECRET,
+    bucket: 'balancer-team-bucket'
+  };
   const input = config;
   input.key = key;
   input.data = JSON.stringify(body);
   const result = await fleek.upload(input);
-  ipfsHash = result.hashV0;
+  return result.hashV0;
+}
+
+export async function ipfsPinByHashPinata(ipfsHash) {
+  const pinata = pinataSDK(
+    process.env.PINATA_API_KEY,
+    process.env.PINATA_SECRET_API_KEY
+  );
+  const result = await pinata.pinByHash(ipfsHash, {
+    customPinPolicy: {
+      regions: [
+        { id: 'FRA1', desiredReplicationCount: 2 },
+        { id: 'NYC1', desiredReplicationCount: 2 }
+      ]
+    }
+  });
+  return result.IpfsHash;
+}
+
+export async function ipfsPin(key: string, body) {
+  const ipfsHash = await ipfsPinFleek(key, body);
+  const result = await ipfsPinByHashPinata(ipfsHash);
   return ipfsHash;
 }
 
